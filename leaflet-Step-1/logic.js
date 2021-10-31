a814930f0b2a092c7d1c17a8d856eceed19529e0
Amy Bednarz <amybednarz@Amys-MacBook-Air.local>
2021-10-30 19:18:03 -0500

    changes

--- leaflet-Step-1/logic.js
+++ leaflet-Step-1/logic.js
@@ -1,6 +1,5 @@
 // Store our API endpoint 
-var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
-
+var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
 // Perform a GET request to the query URL/
 d3.json(queryUrl).then(function (data) {
   // Once we get a response, send the data.features object to the createFeatures function.
@@ -27,13 +26,12 @@ function createMap(earthquakes) {
   // Create the base layers.
   var street = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}',{
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    accessToken: API_KEY
   });
 
    // Create the base layers.
    var tectonics = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}',{
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    accessToken: API_KEY
+
   });
 
   var baseMaps = {
@@ -44,51 +42,18 @@ function createMap(earthquakes) {
     // Create an overlay object to hold our overlay.
   var overlayMaps = {
       Earthquakes: earthquakes
-    };
-  
-// // Create our map, giving it the streetmap and earthquakes layers to display on load.
-//   var myMap = L.map("map", {
-//       center: [
-//         47.09, -85.71
-//       ],
-//       zoom: 4,
-//       layers: [street, earthquakes]
-//   });
-//     // Add the layer control to the map.
-//   L.control.layers(baseMaps, overlayMaps, {
-//       collapsed: false
-//   }).addTo(myMap);
-  
-
-
-//Dynamic Legend map 
-
-function getColor(mag) {
-  return mag > 5? ‘#7a0177’ :
-  mag  > 200000? ‘#BD0026’ :
-  mag > 80000? ‘#E31A1C’ :
-  mag > 10000? ‘#FC4E2A’ :
-  mag > 5000 ? ‘#FD8D3C’ :
-  mag > 500 ? ‘#FEB24C’ :
-  d > 0 ? ‘#FED976’ :
-  ‘#FFEDA0’;
-  }
-
-var legend = L.control({position: 'bottomright}) 
-
-var div = L.DomUtil.create(‘div’, ‘info legend’),
-grades = [0,500,5000,10000,80000,200000,5000000];
-
-for (var i = 0; i < grades.length; i++) {
-  div.innerHTML += ‘<i style=”background:’ + getColor(grades[i] + 1) + ‘”></i> ‘ + grades[i] + (grades[i + 1] ? ‘&ndash;’ + grades[i + 1] + ‘<br>’ : ‘+’);
-  }
-  return div;
   };
   
-  
-
-
-legend.onAdd = function (map) { } 
-
-
-  //legend source: igismap  https://www.igismap.com/legend-in-leafletjs-map-with-topojson/</br>
+// Create our map, giving it the streetmap and earthquakes layers to display on load.
+  var myMap = L.map("map", {
+      center: [
+        47.09, -85.71
+      ],
+      zoom: 4,
+      layers: [street, earthquakes]
+  });
+    // Add the layer control to the map.
+  L.control.layers(baseMaps, overlayMaps, {
+      collapsed: false
+  }).addTo(myMap);
+}
